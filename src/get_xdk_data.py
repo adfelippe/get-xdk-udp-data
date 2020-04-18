dfc9cf3fa7a6c25ecb8ed66db786d29eac216585
Anderson Felippe <adfelippe@gmail.com>
2020-04-18 11:49:32 -0300

    Add feature to save file using timestamp as filename

--- src/get_xdk_data.py
+++ src/get_xdk_data.py
@@ -1,6 +1,7 @@
 # Simple Python script to get data sent from the Bosch XDK
 # over UDP
 import socket
+from datetime import datetime
 
 HOST = ''               # Symbolic name meaning all available interfaces
 PORT = 5005             # Port to listen to data
@@ -17,6 +18,15 @@ s.bind((HOST, PORT))
 
 print('\r\n<Press ENTER to create log file and start saving data>')
 key = input("<Press s to stop saving data and close the log file>")
+# Get current data and time
+current_time = datetime.now()
+date_time = current_time.strftime("%d-%m-%Y_%H-%M-%S")
+
+# Set file name and open it
+filename = 'raw_sensor_data_'
+filename += date_time + '.log'
+log_file = open(filename, 'a')
+
 print('\r\nWaiting for data...\r\n')
 timeout_cnt = 0
 
@@ -25,18 +35,17 @@ while 1:
         data, addr = s.recvfrom(BUFFER_SIZE)
         data = data.decode("utf-8")
         print('Data: {}'.format(data))
-        #key = input()
-        #if (key == 's' or key == 'S'):
-        #    break
+        data = data.rstrip(' \t\r\n\0')
+        log_file.write(data + '\n')
         timeout_cnt = 0
-        #TODO: Save data to log file
     except socket.timeout:
-        #TODO: Close and save data log
         print('[WARNING] Socket reception timeout!\r\n')
         timeout_cnt += 1
         if timeout_cnt >= 3:
             print('[ERROR] Maximum timeout reached. Exiting...\r\n')
+            log_file.close()
             break
 
 s.close()
+log_file.close()
 exit()
